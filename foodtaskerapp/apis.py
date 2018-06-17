9dd1332dc3a8f797a90d9054c7ab16432a4a6d42
hin613 <hin_613@hotmail.com>
2018-06-17 22:46:36 +0800

    driver complete order

--- foodtaskerapp/apis.py
+++ foodtaskerapp/apis.py
@@ -161,8 +161,21 @@ def driver_get_latest_order(request):
 
     return JsonResponse({"order": order})
 
+# POST params: access_token, order_id
+@csrf_exempt
 def driver_complete_order(request):
-    return JsonResponse({})
+    access_token = AccessToken.objects.get(token=request.POST.get("access_token"),expires__gt=timezone.now())
+
+    driver = access_token.user.driver
+
+    order = Order.objects.get(
+        id = request.POST["order_id"],
+        driver = driver
+    )
+    order.status = Order.DELIVERED
+    order.save()
+
+    return JsonResponse({"status": "success"})
 
 def driver_get_revenue(request):
     return JsonResponse({})
